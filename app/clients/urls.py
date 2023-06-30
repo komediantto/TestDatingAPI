d06f7a35e2dc38d9ce3cde1477eae115f4439260
komediantto <komediantto@yandex.ru>
2023-06-30 19:24:15 +0200

    Add matching endpoint

---
+++ app/clients/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import client_create, client_list, match
+from rest_framework.authtoken import views
+
+
+urlpatterns = [
+    path('clients/api-token-auth/', views.obtain_auth_token),
+    path('clients/create/', client_create, name='client-create'),
+    path('clients/<int:pk>/match/', match, name='match')
+]
--- app/clients/views.py
+++ app/clients/views.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+from .models import Client, Match
 from .serializers import ClientSerializer
 
 from PIL import Image
+from django.core.mail import send_mail
 
 
 @api_view(['POST'])
@@ -27,3 +29,45 @@ def client_create(request):
                 image.save(avatar.path)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def match(request, pk):
+    if request.method == 'POST':
+        try:
+            client = Client.objects.get(id=pk)
+        except Client.DoesNotExist:
+            return Response({'error': 'Client not found.'},
+                            status=status.HTTP_404_NOT_FOUND)
+        current_client = request.user
+        client.liked_by.add(current_client)
+        if mutual_sympathy(client, current_client):
+            send_mail(
+                'Взаимная симпатия',
+                f'Вы понравились {current_client.first_name}! \
+                    Почта участника: {current_client.email}',
+                'from@example.com', [client.email], fail_silently=False,
+            )
+            send_mail(
+                'Взаимная симпатия',
+                f'Вы понравились {client.first_name}! \
+                    Почта участника: {client.email}',
+                'from@example.com', [current_client.email],
+                fail_silently=False,
+            )
+
+            return Response(
+                {'message': 'Mutual sympathy! Email has been sent.'},
+                status=200)
+
+        return Response({'message': 'Sympathy not mutual.'},
+                        status=status.HTTP_200_OK)
+
+
+def mutual_sympathy(client1, client2):
+    if client1.liked_by.filter(id=client2.id).exists() and \
+            client2.liked_by.filter(id=client1.id).exists():
+        Match.objects.create(client1=client1, client2=client2)
+        return True
+
+    return False
